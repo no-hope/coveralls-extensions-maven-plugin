4a2a676912699689ed5f974b80d4a65dc083c34a
Ketoth Xupack <ketoth.xupack@gmail.com>
2014-03-14 16:50:48 +0700

    fix for hostname in certificate mismatch
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- src/main/java/org/eluder/coveralls/maven/plugin/httpclient/CoverallsClient.java
+++ src/main/java/org/eluder/coveralls/maven/plugin/httpclient/CoverallsClient.java
@@ -12,10 +12,10 @@ package org.eluder.coveralls.maven.plugin.httpclient;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,12 +26,6 @@ package org.eluder.coveralls.maven.plugin.httpclient;
  * %[license]
  */
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.security.Provider;
-import java.security.Security;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpEntity;
@@ -39,14 +33,35 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.codehaus.plexus.util.IOUtil;
 import org.codehaus.plexus.util.StringUtils;
 import org.eluder.coveralls.maven.plugin.ProcessingException;
 import org.eluder.coveralls.maven.plugin.domain.CoverallsResponse;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.cert.X509Certificate;
+
+import static org.apache.http.conn.ssl.SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
 
 public class CoverallsClient {
 
@@ -57,27 +72,27 @@ public class CoverallsClient {
             }
         }
     }
-    
+
     private static final String FILE_NAME = "coveralls.json";
     private static final ContentType MIME_TYPE = ContentType.create("application/octet-stream", "utf-8");
-    
+
     private static final int DEFAULT_CONNECTION_TIMEOUT = 10000;
     private static final int DEFAULT_SOCKET_TIMEOUT = 60000;
-    
+
     private final String coverallsUrl;
     private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
-    
+
     public CoverallsClient(final String coverallsUrl) {
         this(coverallsUrl, createDefaultClient(), new ObjectMapper());
     }
-    
+
     public CoverallsClient(final String coverallsUrl, final HttpClient httpClient, final ObjectMapper objectMapper) {
         this.coverallsUrl = coverallsUrl;
         this.httpClient = httpClient;
         this.objectMapper = objectMapper;
     }
-    
+
     public CoverallsResponse submit(final File file) throws ProcessingException, IOException {
         HttpEntity entity = MultipartEntityBuilder.create()
                 .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
@@ -88,7 +103,7 @@ public class CoverallsClient {
         HttpResponse response = httpClient.execute(post);
         return parseResponse(response);
     }
-    
+
     private CoverallsResponse parseResponse(final HttpResponse response) throws ProcessingException, IOException {
         HttpEntity entity = response.getEntity();
         ContentType contentType = ContentType.getOrDefault(entity);
@@ -108,7 +123,7 @@ public class CoverallsClient {
             IOUtil.close(reader);
         }
     }
-    
+
     private String getResponseErrorMessage(final HttpResponse response, final String message) {
         int status = response.getStatusLine().getStatusCode();
         String reason = response.getStatusLine().getReasonPhrase();
@@ -121,14 +136,48 @@ public class CoverallsClient {
         }
         return errorMessage;
     }
-    
+
     private static HttpClient createDefaultClient() {
-        RequestConfig requestConfig = RequestConfig.custom()
+        final RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectTimeout(DEFAULT_CONNECTION_TIMEOUT)
                 .setSocketTimeout(DEFAULT_SOCKET_TIMEOUT)
                 .build();
-        return HttpClientBuilder.create()
-                .setDefaultRequestConfig(requestConfig)
-                .build();
+
+        final HttpClientBuilder builder = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig);
+
+        try {
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, new TrustManager[] {new TrustAll()}, new SecureRandom());
+            final SSLConnectionSocketFactory factory =
+                    new SSLConnectionSocketFactory(sslContext, ALLOW_ALL_HOSTNAME_VERIFIER);
+            final Registry<ConnectionSocketFactory> registry =
+                    RegistryBuilder.<ConnectionSocketFactory>create()
+                                   .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                                   .register("https", factory)
+                                   .build();
+
+            builder.setConnectionManager(new BasicHttpClientConnectionManager(registry));
+        } catch (KeyManagementException e) {
+            throw new IllegalStateException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return builder.build();
+    }
+
+    private static class TrustAll implements X509TrustManager {
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        @Override
+        public void checkClientTrusted(final X509Certificate[] certs, final String type) {
+        }
+
+        @Override
+        public void checkServerTrusted(final X509Certificate[] certs, final String authType) {
+        }
     }
 }
